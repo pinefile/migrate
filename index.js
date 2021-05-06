d8dc23b77d24d5099d3e96d0876486491012a926
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2021-05-06 23:09:00 +0200

    Update to use new runCLI api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { runCLI } = require('@pinefile/pine');
+const { api } = require('@pinefile/pine');
 const argv = process.argv.slice(2);
 
 argv.push(`--file=${__dirname}/pinefile.js`);
 
-runCLI(argv);
+api.runCLI(argv);
